91f8812f92791cf9c7f734010196bc84ade5afc7
Guillermo Rodriguez <guillermor@fing.edu.uy>
2020-11-18 12:24:34 -0300

    Change telosprofile include path from "profile" to "tprofile" due to conflict with actual profile table Add link table & create action (addlink)

--- include/vapaee/tprofile/modules/core.hpp
+++ include/vapaee/tprofile/modules/core.hpp
@@ -1,12 +1,12 @@
 #pragma once
 #include <vapaee/base/base.hpp>
-#include <vapaee/profile/tables.all.hpp>
+#include <vapaee/tprofile/tables.all.hpp>
 
 namespace vapaee {
     namespace tprofile {
         namespace core {
 
-            void action_add_profile(name owner, std::string alias) {
+            void action_add_profile(name owner, string alias) {
                 require_auth(owner);
 
                 profiles prof_table(contract, contract.value);
@@ -23,7 +23,7 @@ namespace vapaee {
                 });
             }
 
-            void action_purge_profile(std::string alias) {
+            void action_purge_profile(string alias) {
                 profiles prof_table(contract, contract.value);
 
                 auto alias_index = prof_table.get_index<"alias"_n>();
@@ -33,6 +33,34 @@ namespace vapaee {
                 require_auth(profile_iter->owner);
 
                 alias_index.erase(profile_iter);
+            }
+
+            void action_add_link(
+                string alias,
+                name platform,
+                string url
+            ) {
+                profiles prof_table(contract, contract.value);
+
+                auto alias_index = prof_table.get_index<"alias"_n>();
+                auto profile_iter = alias_index.find(vapaee::utils::hash(alias));
+                check(profile_iter != alias_index.end(), "profile not found");
+
+                name owner = profile_iter->owner;
+
+                require_auth(owner);
+
+                string token = vapaee::utils::prng_token(12);
+
+                links link_table(contract, owner.value);
+                link_table.emplace(owner, [&]( auto& row ) {
+                    row.link_id = link_table.available_primary_key();
+                    row.platform = platform;
+                    row.url = url;
+                    row.token = token;
+                });
+
+                print(token);
             }
 
         };     
--- tests/telosprofile/constants.py
+++ tests/telosprofile/constants.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python3
+
+import string
+import random
+
+
 class TelosProfile:
 	contract_name = "telosprofile"
+	platform_names = [
+		"facebook",
+	    "twitter",
+	    "instagram",
+	    "youtube",
+	    "reddit"
+	]
+
+	def new_profile(eosio_testnet):
+	    account = eosio_testnet.new_account()
+	    alias = ''.join(
+	        random.choice(string.ascii_lowercase + string.digits)
+	        for _ in range(256)
+	    )
+
+	    ec, out = eosio_testnet.push_action(
+	        TelosProfile.contract_name,
+	        'addprofile',
+	        [account, alias],
+	        f'{account}@active'
+	    )
+	    assert ec == 0
+	    return account, alias
