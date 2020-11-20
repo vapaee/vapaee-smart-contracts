b8f1abe835062e64d300ddeb8cbdfaad385d02a7
Guillermo Rodriguez <guillermor@fing.edu.uy>
2020-11-20 11:04:58 -0300

    Add link global platform counter

--- contracts/telosprofile/telosprofile.hpp
+++ contracts/telosprofile/telosprofile.hpp
@@ -9,6 +9,37 @@ namespace vapaee {
     using namespace tprofile;
 
     CONTRACT telosprofile : public eosio::contract {
+
+        /* TODO:
+            global:
+                * link platform counter:
+                  keeps track of how many links are registered
+                  for each platform globaly
+
+            profile:
+                * link list
+                  each link contains:
+                        * platform score
+
+                        * witness list
+                          witnesses are other profiles that certify that
+                          a link is valid
+
+                * KYC level
+                  0 - 20, 0 means noob, 20 is celebrity status
+
+                * raw elo
+                  1+, this is the raw number used to calculate
+                  the KYC level, its calculated using a weighted average.
+    
+                * witness slots
+                  5+, this are slots where users can select from their
+                  link witnesses profiles with the highest elo score.
+
+                  each profile raw elo is a weighted average from their
+                  witness slots
+        */
+
     
         private:
 
@@ -17,11 +48,15 @@ namespace vapaee {
         public:
             using contract::contract;
 
-            ACTION addprofile(name owner, std::string alias) {
+            ACTION addplatform(name platform) {
+                core::action_add_platform(platform);
+            }
+
+            ACTION addprofile(name owner, string alias) {
                 core::action_add_profile(owner, alias);
             }
 
-            ACTION purgeprofile(std::string alias) {
+            ACTION purgeprofile(string alias) {
                 core::action_purge_profile(alias);
             }
 
--- include/vapaee/tprofile/modules/core.hpp
+++ include/vapaee/tprofile/modules/core.hpp
@@ -6,17 +6,29 @@ namespace vapaee {
     namespace tprofile {
         namespace core {
 
+            void action_add_platform(name platform) {
+                require_auth(contract);
+
+                platforms plat_table(contract, contract.value);
+                auto plat_iter = plat_table.find(platform);
+                check(plat_iter == plat_table.end(), "identical platform exists");
+
+                plat_table.emplace(contract, [&](auto& row) {
+                    row.id = platform;
+                    row.counter = 0;
+                });
+            }
+
             void action_add_profile(name owner, string alias) {
                 require_auth(owner);
 
                 profiles prof_table(contract, contract.value);
 
-                // check if the exact same account exists
                 auto alias_index = prof_table.get_index<"alias"_n>();
                 auto profile_iter = alias_index.find(vapaee::utils::hash(alias));
                 check(profile_iter == alias_index.end(), "identical profile exists");
 
-                prof_table.emplace(owner, [&]( auto& row ) {
+                prof_table.emplace(owner, [&](auto& row) {
                     row.id = prof_table.available_primary_key();
                     row.owner = owner;
                     row.alias = alias;
@@ -53,7 +65,7 @@ namespace vapaee {
                 string token = vapaee::utils::prng_token(12);
 
                 links link_table(contract, owner.value);
-                link_table.emplace(owner, [&]( auto& row ) {
+                link_table.emplace(owner, [&](auto& row) {
                     row.link_id = link_table.available_primary_key();
                     row.platform = platform;
                     row.url = url;
