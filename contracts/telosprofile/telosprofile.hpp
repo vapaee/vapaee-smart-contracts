dab6a52b59cb2fc502b3ab1549bd3fcc5d274bd3
Guillermo Rodriguez <guillermor@fing.edu.uy>
2020-11-21 11:16:38 -0300

    Add meet

--- contracts/telosprofile/telosprofile.hpp
+++ contracts/telosprofile/telosprofile.hpp
@@ -10,37 +10,6 @@ namespace vapaee {
 
     CONTRACT telosprofile : public eosio::contract {
 
-        /* TODO:
-            global:
-                * link platform counter:
-                  keeps track of how many links are registered
-                  for each platform globaly
-
-            profile:
-                * link list
-                  each link contains:
-                        * platform score
-
-                        * witness list
-                          witnesses are other profiles that certify that
-                          a link is valid
-
-                * KYC level
-                  0 - 20, 0 means noob, 20 is celebrity status
-
-                * raw elo
-                  1+, this is the raw number used to calculate
-                  the KYC level, its calculated using a weighted average.
-    
-                * witness slots
-                  5+, this are slots where users can select from their
-                  link witnesses profiles with the highest elo score.
-
-                  each profile raw elo is a weighted average from their
-                  witness slots
-        */
-
-    
         private:
 
 #include <vapaee/tprofile/tables.all.hpp>
