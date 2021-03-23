b42bf50c1cc2c585119ac1c704f3890031318347
Guillermo Rodriguez <guillermor@fing.edu.uy>
2021-03-23 12:12:24 -0300

    Add savetoken & setcurrency ballot tests

--- tests/telosbookdex/ballots/test_setcurrency.py
+++ tests/telosbookdex/ballots/test_setcurrency.py
@@ -1,121 +1,31 @@
 #!/usr/bin/env python3
 
-import time
-import random
-
-from pytest_eosiocdt import random_token_symbol
 from pytest_eosiocdt.telos import telosdecide
-from pytest_eosiocdt.sugar import name_to_string
 
 from ..constants import telosbookdex
 
 
 def test_ballot_on_setcurrency_yes(telosdecide, telosbookdex):
-    vote_symbol, vote_precision, vote_supply = telosbookdex.init_dao(telosdecide)
-    token_acc = telosbookdex.testnet.new_account()
-    _, token_acc_id = telosbookdex.new_client(admin=token_acc)
-    ballot_acc = telosbookdex.testnet.new_account()
-    telosdecide.register_voter(
-        ballot_acc, f'{vote_precision},{vote_symbol}', ballot_acc
-    )
-    telosbookdex.testnet.give_token(
-        ballot_acc,
-        '100.0000 TLOS'
-    )
-    telosbookdex.deposit(ballot_acc, '10.0000 TLOS')
+    sym, prec, token_acc, token_acc_id = telosbookdex.init_test_token()
 
-    max_supply = 1000000
-    precision = 6
-    symbol = random_token_symbol()
-    str_max_supply = format(max_supply, f'.{precision}f')
-    str_max_supply = f'{str_max_supply} {symbol}'
-    
-    telosbookdex.testnet.create_token(token_acc, str_max_supply)
-    telosbookdex.testnet.issue_token(token_acc, str_max_supply, '')
-    telosbookdex.add_token(
-        token_acc,
-        'eosio.token',
-        symbol,
-        precision,
-        token_acc
-    )
-
-    voting_power = 1000
-    voting_amount = format(voting_power, f'.{vote_precision}f')
-    def init_voter():
-        voter = telosbookdex.testnet.new_account()
-        telosdecide.register_voter(
-            voter, f'{vote_precision},{vote_symbol}', voter
-        )
-        telosdecide.mint(
-            voter,
-            f'{voting_amount} {vote_symbol}',
-            f'bantoken {symbol} eosio.token'
-        )
-        return voter
-
-    total_voters = 10
-    voters = [
-        init_voter()
-        for _ in range(total_voters)
-    ]
-
-    ec, out = telosbookdex.dao_setcurrency(
-        symbol,
-        'eosio.token',
-        ballot_acc
-    )
-
-    assert ec == 0
-
-    start_time = time.time()
-
-    ballot_info = next(
-        (row
-        for row in telosbookdex.get_ballots()
-        if row['operation'] == 'setcurrency' and
-        len(row['params']) == 2 and
-        row['params'][0] == symbol and
-        row['params'][1] == 'eosio.token' and
-        row['feepayer'] == ballot_acc),
-        None
-    )
-    assert ballot_info is not None
-
-    options = [*[['yes'] for _ in range(8)], ['no']]
-    ballot_name = name_to_string(ballot_info['id'])
-    current_time = 0
-    for voter in voters:
-        current_time = time.time()
-        if (current_time - start_time) > 3:
-            break
-        ec, _ = telosdecide.cast_vote(
-            voter, ballot_name, random.choice(options)
+    with telosbookdex.perform_vote(
+        telosdecide,
+        [['yes']]
+    ) as vote_info:
+        voters, ballot_acc  = vote_info
+        ec, out = telosbookdex.dao_setcurrency(
+            sym,
+            'eosio.token',
+            ballot_acc
         )
 
-    remaining = 5 - (current_time - start_time)
-    if remaining > 0:
-        time.sleep(remaining + 1)
+        assert ec == 0
 
-    ec, _ = telosdecide.close_voting(ballot_name)
-    assert ec == 0
-
-    ballot_info = next(
-        (row
-        for row in telosbookdex.get_ballots()
-        if row['id'] == ballot_info['id']),
-        None
-    )
+    ballot_info = telosbookdex.get_ballot_by_feepayer(ballot_acc) 
 
     assert ballot_info is not None
     assert ballot_info['approved'] == 1
     assert ballot_info['accepted'] == 1
-
-    total_vote = 0
-    for option in ballot_info['results']:
-        total_vote += float(option['value'].split(' ')[0])
-
-    assert total_vote == total_voters * voting_power
 
     breakpoint()
 
