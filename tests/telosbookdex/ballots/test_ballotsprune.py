8567f0b9dc59d62c030c63eab5f20f9319ea3bcf
Guillermo Rodriguez <guillermor@fing.edu.uy>
2021-04-12 12:46:48 -0300

    Add remanining ballot tests

---
+++ tests/telosbookdex/ballots/test_ballotsprune.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from pytest_eosiocdt import name_to_string
+from pytest_eosiocdt.telos import telosdecide
+
+from ..constants import telosbookdex
+
+
+def test_ballot_on_ballotsprune_yes(telosdecide, telosbookdex):
+
+    max_entries = 500
+
+    with telosbookdex.perform_vote(
+        telosdecide,
+        [['yes']]
+    ) as vote_info:
+        voters, ballot_acc  = vote_info 
+        ec, out = telosbookdex.dao_ballotsprune(
+            max_entries,
+            ballot_acc
+        )
+
+        assert ec == 0
+
+    ballot_info = telosbookdex.get_ballot_by_feepayer(ballot_acc) 
+
+    assert ballot_info is not None
+    assert ballot_info['approved'] == 1
+    assert ballot_info['accepted'] == 1
+
+    config = telosbookdex.get_config()
+
+    assert int(config['bprune']) == max_entries
+
+
+def test_ballot_on_ballotsprune_no(telosdecide, telosbookdex):
+
+    old_max = int(telosbookdex.get_config()['bprune'])
+    new_max = 500
+
+    with telosbookdex.perform_vote(
+        telosdecide,
+        [['no']]
+    ) as vote_info:
+        voters, ballot_acc  = vote_info 
+        ec, out = telosbookdex.dao_ballotsprune(
+            new_max,
+            ballot_acc
+        )
+
+        assert ec == 0
+
+    ballot_info = telosbookdex.get_ballot_by_feepayer(ballot_acc) 
+
+    assert ballot_info is not None
+    assert ballot_info['approved'] == 0
+    assert ballot_info['accepted'] == 1
+
+    config = telosbookdex.get_config()
+
+    assert int(config['bprune']) == old_max
---
+++ tests/telosbookdex/ballots/test_eventsprune.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from pytest_eosiocdt import name_to_string
+from pytest_eosiocdt.telos import telosdecide
+
+from ..constants import telosbookdex
+
+
+def test_ballot_on_eventsprune_yes(telosdecide, telosbookdex):
+
+    days = 10
+
+    with telosbookdex.perform_vote(
+        telosdecide,
+        [['yes']]
+    ) as vote_info:
+        voters, ballot_acc  = vote_info 
+        ec, out = telosbookdex.dao_eventsprune(
+            days,
+            ballot_acc
+        )
+
+        assert ec == 0
+
+    ballot_info = telosbookdex.get_ballot_by_feepayer(ballot_acc) 
+
+    assert ballot_info is not None
+    assert ballot_info['approved'] == 1
+    assert ballot_info['accepted'] == 1
+
+    config = telosbookdex.get_config()
+
+    assert int(config['eprune']) == days
+
+
+def test_ballot_on_eventsprune_no(telosdecide, telosbookdex):
+
+    old_days = int(telosbookdex.get_config()['eprune'])
+    new_days = 10
+
+    with telosbookdex.perform_vote(
+        telosdecide,
+        [['no']]
+    ) as vote_info:
+        voters, ballot_acc  = vote_info 
+        ec, out = telosbookdex.dao_eventsprune(
+            new_days,
+            ballot_acc
+        )
+
+        assert ec == 0
+
+    ballot_info = telosbookdex.get_ballot_by_feepayer(ballot_acc) 
+
+    assert ballot_info is not None
+    assert ballot_info['approved'] == 0
+    assert ballot_info['accepted'] == 1
+
+    config = telosbookdex.get_config()
+
+    assert int(config['eprune']) == old_days
