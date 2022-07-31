ddeda1ec10b29b9481d5ead644be3f839d426e2c
Viterbo <viter.rod@gmail.com>
2022-07-31 17:30:09 -0300

    closes #62

--- include/vapaee/token/modules/issuance.hpp
+++ include/vapaee/token/modules/issuance.hpp
@@ -11,14 +11,14 @@ namespace vapaee {
                 return vapaee::token::contract;
             }
 
-            void action_chissuer(const name& new_issuer, const symbol_code& symcode) {
+            void action_chissuer(const name& new_issuer, const symbol_code& token) {
                 PRINT("vapaee::token::issuance::action_chissuer()\n");
                 PRINT(" new_issuer: ", new_issuer.to_string(), "\n");
-                PRINT(" symcode: ", symcode.to_string(), "\n");
+                PRINT(" token: ", token.to_string(), "\n");
 
-                stats statstable( get_self(), symcode.raw() );
-                auto ptr = statstable.find( symcode.raw() );
-                check( ptr != statstable.end(), "[chissuer] token with symcode does not exist, create token before issue (vapaeetokens)" );
+                stats statstable( get_self(), token.raw() );
+                auto ptr = statstable.find( token.raw() );
+                check( ptr != statstable.end(), "[chissuer] token with token does not exist, create token before issue (vapaeetokens)" );
 
                 require_auth(ptr->issuer);
 
@@ -26,7 +26,46 @@ namespace vapaee {
                     s.issuer = new_issuer;
                 });
                 
-                PRINT("vapaee::token::issuance::action_chissuer()...\n");
+            }
+            
+            void action_issuer(const name& action, const name& theissuer, const asset& max) {
+                PRINT("vapaee::token::issuance::action_issuer()\n");
+                PRINT(" action: ", action.to_string(), "\n");
+                PRINT(" theissuer: ", theissuer.to_string(), "\n");
+                PRINT(" max: ", max.to_string(), "\n");
+
+                symbol_code token = max.symbol.code();
+                PRINT(" > token: ", token.to_string(), "\n");
+
+                stats statstable( get_self(), token.raw() );
+                auto ptr = statstable.find( token.raw() );
+                check( ptr != statstable.end(), "[chissuer] token with token does not exist, create token before issue (vapaeetokens)" );
+
+                require_auth(ptr->issuer);
+                name ram_payer = ptr->issuer;
+
+                issuers issuerstable( get_self(), token.raw() );
+                auto iss_ptr = issuerstable.find( theissuer.value );
+
+                if (action == name("add")) {
+                    if (iss_ptr == issuerstable.end()) {
+                        issuerstable.emplace(ram_payer, [&]( auto& a ){
+                            a.issuer = theissuer;
+                            a.max    = max;
+                        });
+                    } else {
+                        issuerstable.modify(*iss_ptr, ram_payer, [&]( auto& a ){
+                            a.max    += max;
+                        });
+                    }
+                } else if (action == name("remove")) {
+                    check(iss_ptr != issuerstable.end(), 
+                        create_error_string2("ERR-AIS-01: can't remove issuer for token. It has not found. (the issuer, token) ",
+                            theissuer.to_string(),
+                            token.to_string()
+                        ).c_str()
+                    );
+                }
             }
 
         };     
