630aed5bcd3af47dee529811549c417e9084338f
Viterbo <viter.rod@gmail.com>
2022-08-24 21:38:28 -0300

    vapaeepayhub-1.1.8 - adding recent history to staking tables

--- include/vapaee/pay/tables/stakepools.hpp
+++ include/vapaee/pay/tables/stakepools.hpp
@@ -55,7 +55,17 @@ struct payhub_target {
     };
 };
 //---------------------------------------
+struct stakepool_snapshot {
+    time_point_sec date;                // date of the snapshot
+    asset pool_stake;                   // Total amount of tokens in staking in this pool
+    asset pool_funds;                   // Total amount of real tokens for this pool. pool_funds = pool_stake + droponpool()
+    asset pool_rex;                     // Total amount of rex of this pool
 
+    std::string to_string() const {
+        return string("stakepool [date:") + std::to_string((unsigned long)date.utc_seconds) + " pool_stake:" + pool_stake.to_string() + " pool_funds:" + pool_funds.to_string() + " pool_rex:" + pool_rex.to_string() + "]";
+    };
+    
+};
 
 TABLE stakepool_table {
     // pool id
@@ -69,6 +79,9 @@ TABLE stakepool_table {
     asset pool_stake;                   // Total amount of tokens in staking in this pool
     asset pool_funds;                   // Total amount of real tokens for this pool. pool_funds = pool_stake + droponpool()
     asset pool_rex;                     // Total amount of rex of this pool
+
+    // vector of recent snapshots
+    vector<stakepool_snapshot> history; // recent history of the pool
 
     uint64_t primary_key()const { return pool.id.value; }
 
